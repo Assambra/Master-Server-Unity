bca4fe780f8a662bc627bee9aa675c0ead1cb9d4
Philipp Leutner <philipp.leutner@assambra.com>
2024-03-29 20:00:36 +0100

    The NetworkManager for Unity-Client and Unity-Server, Added the Persistent Scene to Scenes in Build

---
+++ Unity-Client/Assets/Client/Scripts/Managers/NetworkManager.cs
@@ -0,0 +1,47 @@
+using com.tvd12.ezyfoxserver.client;
+using com.tvd12.ezyfoxserver.client.unity;
+
+
+namespace Assambra.Client
+{
+    public class NetworkManager : EzyAbstractController
+    {
+        public static NetworkManager Instance { get; private set; }
+
+        private EzySocketConfig socketConfig;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+                Destroy(this);
+            else
+                Instance = this;
+
+            socketConfig = GetSocketConfig();
+        }
+
+        private new void OnEnable()
+        {
+            base.OnEnable();
+        }
+
+        private void Update()
+        {
+            EzyClients.getInstance()
+                .getClient(socketConfig.ZoneName)
+                .processEvents();
+        }
+
+        protected override EzySocketConfig GetSocketConfig()
+        {
+            return EzySocketConfig.GetBuilder()
+                .ZoneName("master-server")
+                .AppName("master-server")
+                .TcpUrl("127.0.0.1:3005")
+                .UdpPort(2611)
+                .UdpUsage(true)
+                .EnableSSL(false)
+                .Build();
+        }
+    }
+}
