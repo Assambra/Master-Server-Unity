30756c0cc17d294c8d00696b24e052c5fabd902d
Philipp Leutner <philipp.leutner@assambra.com>
2024-03-31 09:48:56 +0200

    New utility class getAlphaNumericString to create server passwords

---
+++ master-server/game-box-master-server-unity/src/main/java/com/assambra/gameboxmasterserverunity/utils/RandomString.java
@@ -0,0 +1,20 @@
+package com.assambra.gameboxmasterserverunity.utils;
+
+public class RandomString {
+
+    public static String getAlphaNumericString(int n)
+    {
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
+
+        StringBuilder sb = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            int index = (int)(AlphaNumericString.length() * Math.random());
+
+            sb.append(AlphaNumericString.charAt(index));
+        }
+
+        return sb.toString();
+    }
+}
