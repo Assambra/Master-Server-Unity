e06f76c6ebaabbfa80e170e7046f1a0e60450e17
Philipp Leutner <philipp.leutner@assambra.com>
2024-04-01 18:44:20 +0200

    RandomStringUtil to generate random strings eg server passwords, activation codes

---
+++ master-server/master-server-app-api/src/main/java/com/assambra/app/masterserver/util/RandomStringUtil.java
@@ -0,0 +1,20 @@
+package com.assambra.app.masterserver.util;
+
+public class RandomStringUtil {
+
+    public static String getAlphaNumericString(int n)
+    {
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789" + "abcdefghijklmnopqrstuvxyz";
+
+        StringBuilder sb = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            int index = (int)(AlphaNumericString.length() * Math.random());
+
+            sb.append(AlphaNumericString.charAt(index));
+        }
+
+        return sb.toString();
+    }
+}
