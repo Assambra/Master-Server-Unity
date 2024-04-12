ce9fc6aee07da935b8b2636f8e4892031de7180a
Philipp Leutner <philipp.leutner@assambra.com>
2024-04-12 20:07:40 +0200

    New Scene CreatePlayer, New UICreatePlayer prefab and UICreatePlayer script alle nessassary steps to integrate the scene in our Module-GameManager

---
+++ Unity-Client/Assets/Client/Scripts/UserInterfaces/UICreatePlayer.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class UICreatePlayer : MonoBehaviour
+{
+
+    public void OnButtonCreate()
+    {
+        Debug.Log("Button Create clicked");
+    }
+
+    public void OnButtonBack()
+    {
+        Debug.Log("Button Back clicked");
+    }
+}
