ae001cd70883569192261290f3a94d757f313445
Philipp Leutner <philipp.leutner@assambra.com>
2024-04-12 19:46:59 +0200

    New enums Scenes for CreatePlayer and SelectPlayer

--- Unity-Client/Assets/Assambra/Game Framework/Modules/GameManager/Scripts/Enums/Scenes.cs
+++ Unity-Client/Assets/Assambra/Game Framework/Modules/GameManager/Scripts/Enums/Scenes.cs
@@ -4,8 +4,10 @@ namespace Assambra.GameFramework.GameManager
     public enum Scenes
     {
         Login = 0,
-        Newcomer = 1,
-        World = 2,
-        BossRoom = 3
+        CreatePlayer = 1,
+        SelectPlayer = 2,
+        Newcomer = 3,
+        World = 4,
+        BossRoom = 5
     }
 }
